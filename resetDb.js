300b25e6f4416d07c71c21b15cc1d4e6e2108b79
angel juarez <angeljuarez729@gmail.com>
2018-12-10 14:55:08 -0500

    eslint finish

--- resetDb.js
+++ resetDb.js
@@ -3,13 +3,15 @@ const { sequelize } = require('./models');
 
 async function resetDb() {
   try {
-    await sequelize.sync({force: true});
+    await sequelize.sync({ force: true });
+    // eslint-disable-next-line no-console
     console.log('database sync\'d');
   } catch (e) {
+    // eslint-disable-next-line no-console
     console.error(e);
   } finally {
     process.exit();
   }
 }
 
-resetDb()
+resetDb();
