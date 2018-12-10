b45b9d2b8ec92dba8b515d9559b6b36bd97b1f3b
angel juarez <angeljuarez729@gmail.com>
2018-12-10 11:18:37 -0500

    added packages

---
+++ resetDb.js
@@ -0,0 +1,15 @@
+// taken from a random repo that you gave us
+const { sequelize } = require('./models');
+
+async function resetDb() {
+  try {
+    await sequelize.sync({force: true});
+    console.log('database sync\'d');
+  } catch (e) {
+    console.error(e);
+  } finally {
+    process.exit();
+  }
+}
+
+resetDb();
