d339f7500e072e3b1cb29f0f31f4857efc477af8
student_123 <student_123@ool-ad020780.dyn.optonline.net>
2018-12-10 14:32:52 -0500

    added esLint

--- resetDb.js
+++ resetDb.js
@@ -3,7 +3,7 @@ const { sequelize } = require('./models');
 
 async function resetDb() {
   try {
-    await sequelize.sync({force: true});
+    await sequelize.sync({ force: true });
     console.log('database sync\'d');
   } catch (e) {
     console.error(e);
