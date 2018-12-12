785a3c4f2ae19ef17e51d609ef98f8fd67528d01
student_123 <student_123@ool-ad020780.dyn.optonline.net>
2018-12-12 09:59:55 -0500

    fixed linter errors

--- routes/users.js
+++ routes/users.js
@@ -6,78 +6,85 @@ const { Recipe } = require('../models');
 
 const userRouter = express();
 
-userRouter.use((req, res, next) =>{
+userRouter.use((req, res, next) => {
+  // eslint-disable-next-line no-console
   console.log('using user Router');
   next();
-})
+});
 
 userRouter.get('/', async (req, res) => {
-  try{
+  try {
     const users = await User.findAll();
     res.json(users);
-  }catch(e){
+  } catch (e) {
+    // eslint-disable-next-line no-console
     console.log(e);
-    res.status(500).json({message: e.message});
+    res.status(500).json({ message: e.message });
   }
-})
+});
 
 userRouter.get('/:id', async (req, res) => {
-  try{
-    const thisUser = await User.findByPk(req.params.id)
-    res.json(thisUser)
-  }catch (e){
+  try {
+    const thisUser = await User.findByPk(req.params.id);
+    res.json(thisUser);
+  } catch (e) {
+    // eslint-disable-next-line no-console
     console.log(e);
-    res.status(500).json({message: e.message})
+    res.status(500).json({ message: e.message });
   }
 });
 
 userRouter.get('/:id/favorites', async (req, res) => {
-try {
-  const recipe = await Recipe.findAll({
-    include:[{
-      model: User,
-      attributes:[],
-      where:{id: req.params.id}
-    }]});
-  res.json(recipe);
-}
-catch(e) {
-  console.log(e);
-  res.status(500).json({ message: e.message });
-}
-});
-
-userRouter.post('/', async (req, res) => {
-  try{
-    const newUser = await User.create(req.body);
-    res.json(newUser);
-  }catch(e){
-  console.error(e);
-  res.status(500).json({message:e.message})
+  try {
+    const recipe = await Recipe.findAll({
+      include: [{
+        model: User,
+        attributes: [],
+        where: { id: req.params.id },
+      }],
+    });
+    res.json(recipe);
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e);
+    res.status(500).json({ message: e.message });
   }
 });
 
-  userRouter.delete('/:id', async (req, res) => {
-    try{
-      const deleteThis = await User.destroy({where: {id: req.params.id}});
-      res.json(deleteThis)
-    }catch(e){
-      console.error(e);
-      res.status(500).json({message:e.message});
-    }
-  })
+userRouter.post('/', async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.json(newUser);
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+    res.status(500).json({ message: e.message });
+  }
+});
 
-  userRouter.put('/:id', async (req, res) => {
-    try{
-      const info = req.body;
-      const user = await User.findByPk(req.params.id);
-      user.update(info);
-      res.json(user);
-    }catch(e){
-      console.error(e);
-      res.status(500).json({message: e.message});
-    }
-  });
+userRouter.delete('/:id', async (req, res) => {
+  try {
+    const deleteThis = await User.destroy({ where: { id: req.params.id } });
+    res.json(deleteThis);
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+    res.status(500).json({ message: e.message });
+  }
+});
+
+userRouter.put('/:id', async (req, res) => {
+  try {
+    const info = req.body;
+    const user = await User.findByPk(req.params.id);
+    user.update(info);
+    res.json(user);
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+    res.status(500).json({ message: e.message });
+  }
+});
 
 
 // userRouter.get('/:id', async (req, res) => {
@@ -91,5 +98,5 @@ userRouter.post('/', async (req, res) => {
 // });
 
 module.exports = {
-  userRouter
+  userRouter,
 };
