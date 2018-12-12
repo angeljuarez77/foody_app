1bd6c8fc3a760d838e269b0d0356c92f89a35a27
student_123 <student_123@ool-ad020780.dyn.optonline.net>
2018-12-12 10:29:07 -0500

    added login route to user route file

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
-
+const { passport, sign } = require('../auth');
+const bcrypt = require('bcrypt');
 const { User } = require('../models');
 const { Recipe } = require('../models');
 
@@ -63,6 +63,28 @@ userRouter.post('/', async (req, res) => {
   } catch (e) {
     // eslint-disable-next-line no-console
     console.error(e);
+    res.status(500).json({ message: e.message });
+  }
+});
+
+userRouter.post('/login', async (req, res) => {
+  try {
+    const { name, password } = req.body;
+    const user = await User.find({ where: { name } });
+    const passwordValid = await bcrypt.compare(password, user.password);
+    const { id } = user;
+    if (passwordValid) {
+      const token = sign({
+        id,
+        name,
+      });
+      res.json({ token });
+    } else {
+      throw Error('Invalid credentials');
+    }
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e);
     res.status(500).json({ message: e.message });
   }
 });
