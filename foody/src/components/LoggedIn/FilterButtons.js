4a503f0612ae7dbb8c45c239d5b5428b21147bfe
J3SS13 <jessicamclaughlan@me.com>
2018-12-13 15:41:12 -0500

    changed button colors

--- foody/src/components/LoggedIn/FilterButtons.js
+++ foody/src/components/LoggedIn/FilterButtons.js
@@ -7,9 +7,9 @@ return(
   <div>
       <Button onClick={()=>props.handleSelect('Vegan')} key='1' outline color="primary"> Vegan </Button>
       <Button onClick={()=>props.handleSelect('Vegetarian')} key='2' outline color="primary"> Vegetarian </Button>
-      <Button onClick={()=>props.handleSelect('')} key="3" outline color="primary"> Reset Filters </Button>
+      <Button onClick={()=>props.handleSelect('')} key="3" outline color="secondary"> Reset Filters </Button>
       <Button onClick={()=>props.renderFavorites(!props.favoritesView)} outline color="danger"> Favorites </Button>
-      <Button id='add-recipe'>Add Recipe </Button>
+      <Button outline color="success">Add Recipe </Button>
   </div>
   )
 };
